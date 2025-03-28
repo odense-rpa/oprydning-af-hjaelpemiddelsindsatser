2c50acb17612a5c13f983d14567b8a1a80e5b0ed
odensesimof <simof@odense.dk>
2025-03-28 10:22:11 +0100

    Implementeret og testet processen internt - afventer officiel test.

--- main.py
+++ main.py
@@ -2,44 +2,185 @@ import asyncio
 import logging
 import sys
 
-from automation_server_client import AutomationServer, Workqueue, WorkItemError
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from automation_server_client import AutomationServer, Workqueue, WorkItemError, Credential
+from kmd_nexus_client import NexusClient, CitizensClient, OrganizationsClient, GrantsClient, filter_references
+from odk_tools.tracking import Tracker
+from odk_tools.reporting import Reporter
+
+nexus_client: NexusClient = None
+citizens_client: CitizensClient = None
+organizations_client: OrganizationsClient = None
+grants_client: GrantsClient = None
+tracker: Tracker = None
+reporter: Reporter = None
+logger = None
+process_name = "Oprydning af hjælpemiddelsindsatser"
 
 
-async def populate_queue(workqueue: Workqueue):
-    logger = logging.getLogger(__name__)
+def populate_queue(workqueue: Workqueue):
+    organization = organizations_client.get_organization_by_name("Hjælpemiddelservice")
+    citizens = organizations_client.get_citizens_by_organization(organization)
 
-    logger.info("Hello from populate workqueue!")
+    for citizen in citizens:        
+        try:            
+            cpr =  citizen["patientIdentifier"]["identifier"]            
+            workqueue.add_item({"Cpr": cpr}, cpr)
+        except Exception as e:
+            logging.error(f"Error adding citizen to Queue {citizen}: {e}")
 
 
 async def process_workqueue(workqueue: Workqueue):
-    logger = logging.getLogger(__name__)
-
-    logger.info("Hello from process workqueue!")
-
     for item in workqueue:
         with item:
             data = item.get_data_as_dict()
-
+            data = citizens_client.get_citizen(data.get("Cpr"))
+            
             try:
-                # Process the item here
-                pass
+                report_lendings_without_basket_grants = check_lendings_without_basket_grants(data)
+                manage_related_basket_grants(data)
+                remove_relation(data)
+
+                if report_lendings_without_basket_grants:
+                    reporter.report(process_name, "Borgere med udlån uden indsats", {'Cpr': data.get("Cpr")})
+
+                tracker.track_task(process_name)        
+
             except WorkItemError as e:
-                # A WorkItemError represents a soft error that indicates the item should be passed to manual processing or a business logic fault
-                logger.error(f"Error processing item: {data}. Error: {e}")
-                item.fail(str(e))
+                logger.warning(f"Error processing work item: {e}")
+
+
+def check_lendings_without_basket_grants(citizen_data: dict) -> bool:
+    lending_without_basket_grant = False
+    lendings = citizens_client.get_citizen_lendings(citizen_data)
+        
+    for lending in lendings:
+        if "grant" not in lending or lending["grant"]["originatorStatus"] == "Afsluttet" or lending["grant"] is None:
+            logging.info(f"Lending found without basket grant: {lending}")
+            lending_without_basket_grant = True
+
+    return lending_without_basket_grant
+
+
+def manage_related_basket_grants(citizen_data: dict):
+    pathway = citizens_client.get_citizen_pathway(citizen_data)
+    basket_grant_references = citizens_client.get_citizen_pathway_references(pathway)    
+    filtered_references = filter_references(
+        basket_grant_references,
+        path = "/Sundhedsfagligt grundforløb/*/Indsatser/basketGrantReference",
+        active_pathways_only = False
+    )
+    
+    for reference in filtered_references:
+        basket_grant = citizens_client.resolve_reference(reference)
+
+        if basket_grant["workflowState"]["name"] in ["Afsluttet", "Annulleret", "Afslået"]:
+            continue
+
+        elements = grants_client.get_grant_elements(basket_grant)
+        
+        if "supplier" not in elements:
+            continue
+
+        if (isinstance(elements.get("supplier"), dict) and isinstance(elements["supplier"].get("supplier"), dict) and elements["supplier"]["supplier"].get("name") == "Hjælpemiddelservice"):
+            active_lending_matching_basket_grant = get_active_lending_matching_basket_grant(citizen_data, basket_grant)
+            
+            if not active_lending_matching_basket_grant:
+                logger.info(f"Trying to edit basket grant with id: {basket_grant['basketGrantId']} for citizen: {citizen_data['patientIdentifier']['identifier']}")
+                inactivate_basket_grant(citizen_data, basket_grant)
+
+
+def get_active_lending_matching_basket_grant(citizen_data: dict, basket_grant: dict) -> bool:
+    lendings = citizens_client.get_citizen_lendings(citizen_data)
+
+    for lending in lendings:
+        if str(basket_grant["basketGrantId"]) == str(lending["grant"]["originatorId"]) or str(basket_grant["currentOrderGrantId"]) == str(lending["grant"]["originatorId"]):
+            return True
+        
+    return False
+
+
+def inactivate_basket_grant(citizen_data: dict, basket_grant: dict):
+    transitions = {
+        "Bestilt": "Afslut",
+        "Bevilliget": "Annullér",
+        "Ændret": "Afslut"
+    }
+
+    if basket_grant["workflowState"] not in transitions:
+        logging.error(f"Kan ikke afslutte indsats på borger {citizen_data['cpr']}")
+        return
+
+    if basket_grant["workflowState"] == "Bevilliget":
+        requested_date = datetime.strptime(basket_grant["workflowRequestedDate"], "%d-%m-%Y")
+        now = datetime.now()
+        one_month_ago = now - relativedelta(months=1)
+        
+        if requested_date < one_month_ago:
+            logging.warning(f"Potentielt aktivt udlån på borger {citizen_data['cpr']}")
+            return        
+
+    field_updates = {}    
+    
+    if(transitions[basket_grant["workflowState"]] == "Afslut"):
+        field_updates = {
+            "billingEndDate": datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + '+0000',
+            "basketGrantEndDate": datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + '+0000'
+        }
+    else:
+        field_updates = {
+            "cancelledDate": datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + '+0000'            
+        }
+    
+    grants_client.edit_grant(basket_grant, field_updates, transitions[basket_grant["workflowState"]])    
+    
+
+def remove_relation(citizen_data: dict):    
+    lendings = citizens_client.get_citizen_lendings(citizen_data)
+    
+    if len(lendings) == 0:
+        organization_name = "Hjælpemiddelservice"
+        organizations = organizations_client.get_organizations_by_citizen(citizen_data)
+        
+        filtered_organization = next(
+            (rel for rel in organizations if rel["organization"]["name"] == organization_name),
+            None
+        )    
+    
+        if filtered_organization is not None:
+            organizations_client.remove_citizen_from_organization(dict(filtered_organization))
 
 
 if __name__ == "__main__":
     ats = AutomationServer.from_environment()
+    workqueue = ats.workqueue()    
+        
+    credential = Credential.get_credential("KMD Nexus - produktion")
+    tracking_credential = Credential.get_credential("Odense SQL Server")
+    reporting_credential = Credential.get_credential("RoboA")
 
-    workqueue = ats.workqueue()
+    nexus_client = NexusClient(client_id=credential.username, client_secret=credential.password, instance=credential.get_data_as_dict()["instance"])
+    citizens_client = CitizensClient(nexus_client=nexus_client)
+    organizations_client = OrganizationsClient(nexus_client=nexus_client)
+    grants_client = GrantsClient(nexus_client=nexus_client)
 
-    # Initialize external systems for automation here..
+    tracker = Tracker(
+        username = tracking_credential.username, 
+        password = tracking_credential.password
+    )
+
+    reporter = Reporter(
+        username = reporting_credential.username, 
+        password = reporting_credential.password
+    )
+
+    logger = logging.getLogger(__name__)        
 
     # Queue management
     if "--queue" in sys.argv:
         workqueue.clear_workqueue("new")
-        asyncio.run(populate_queue(workqueue))
+        populate_queue(workqueue)
         exit(0)
 
     # Process workqueue
